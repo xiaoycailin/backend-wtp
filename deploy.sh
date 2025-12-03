117607d1d894c8f2ca6ad99288d51e151dc928c2
xiaoycaillin <abiaiden220809@gmail.com>
2025-12-03 14:21:47 +0700

    asasas

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ git pull
 echo "Installing dependencies..."
 npm install --omit=dev
 
-echo "Building project..."
-npm run build
+# echo "Building project..."
+# npm run build
 
 echo "Restarting service..."
 pm2 restart marketplaceservice || pm2 start dist/server.js --name marketplaceservice
