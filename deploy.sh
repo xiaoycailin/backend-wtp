98537a4ad4eed1142af9a3102d49be405545ddf6
Aiden <denisprasetio060@gmail.com>
2026-04-13 23:42:19 +0700

    edit deploy sh

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,18 @@ set -e
 
 cd /home/www/backend-wtp
 
-echo "Pulling latest changes..."
-git pull
+echo "Syncing with GitHub..."
+git fetch origin
+git reset --hard origin/main
 
 echo "Installing dependencies..."
 npm install --omit=dev
 
-echo "Generating data types from prisma..."
+echo "Generating prisma client..."
 npx prisma generate
 
-# echo "Building project..."
-# npm run build
+echo "Building project..."
+npm run build
 
 echo "Restarting service..."
 pm2 restart backendwtp || pm2 start node --name "backendwtp" -- -r tsconfig-paths/register dist/index.js
